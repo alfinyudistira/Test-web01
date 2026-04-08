13ed408750ae9a10da7da6cb172d88309a05e7df
ALFIN MAULANA YUDISTIRA <alvinyoalvin44@gmail.com>
2026-04-08 10:56:34 +0700

    Update commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,26 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-case': [2, 'always', 'lower-case'],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'ui', 
+        'components',
+        'modules',
+        'store',  
+        'hooks', 
+        'lib',  
+        'i18n', 
+        'types',  
+        'router',  
+        'config', 
+        'deps'    
+      ],
+    ],
+
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+  },
+};
